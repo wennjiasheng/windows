f28c6f072264fd0b76d53ff2a846d8affac305de
584479748 <584479748@qq.com>
2020-06-08 23:12:05 +0800

    URL Protocol技术

---
+++ AsiaLocalMainSso/AsiaLocalMainSso/AsiaLocalMainSso.h
@@ -0,0 +1,32 @@
+
+// AsiaLocalMainSso.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CAsiaLocalMainSsoApp:
+// 有关此类的实现，请参阅 AsiaLocalMainSso.cpp
+//
+
+class CAsiaLocalMainSsoApp : public CWinApp
+{
+public:
+	CAsiaLocalMainSsoApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CAsiaLocalMainSsoApp theApp;
